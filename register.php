9628d65c5668bddc445bbe6ac955cec18dc1cce1
Gopikamchandran <53312508+Gopikamchandran@users.noreply.github.com>
2021-03-07 18:43:16 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
     require_once('db.php');
     // When form submitted, insert values into the database. -->
     if (isset($_POST['register'])) {
@@ -13,7 +14,7 @@
         $salt="zennode";
         $password=sha1($password.$salt);
 // echo "$password";
-     
+    
 
         $sql="SELECT * from register where (email='$email');";
             $res=mysqli_query($con,$sql);
@@ -21,10 +22,12 @@
             {
 
                   // output data of each row
-                  $row = mysqli_fetch_assoc($res);
-            
-                if($email==$row['email'])
+                 //$row = mysqli_fetch_assoc($res);
+           //$email= $row['email'];
+           //  $_SESSION['user']=$email;
+               // if($email==$row['email'])
                  // if($email = isset($row['email']) ? count($row['email']) : 0)
+              if(mysqli_num_rows($res)>0)
                  {
                       // echo "Email already exists";
                       echo' <script>alert
@@ -36,13 +39,18 @@
        
                       $query    = "INSERT into register (email,password)
                      VALUES (  '$email','$password')";
+                    // $reg=mysqli_query($con,$query);
+                     //$row=mysqli_fetch_assoc($reg);
+                    // $email=$reg['email'];
+                    // $_SESSION['user']=$email;
                        $result   = mysqli_query($con, $query);
+                    //   return false;
                     if ($result)
                      {
                         // to display in the form of a message
                       echo' <script>alert
-                        ("Successfully registerd");window.location.assign("sampleref.php");</script>';
-            
+                        ("Successfully registerd");window.location.assign("time.php");</script>';
+                        
                       } else
                        {
                         echo'  <script>alert
